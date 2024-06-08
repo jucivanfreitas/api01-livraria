8207748b627ff9dd63b6f89ebf04aab67b09c6b0
Jucivan Freitas <42095383+jucivanfreitas@users.noreply.github.com>
2024-06-08 10:11:20 -0300

    Sua mensagem de commit aqui

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Wdsddsadsdsadasdd!');
-});
+app.get('/livros', rotaLivro);
 
 // Start the server
 
